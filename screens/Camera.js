c8b3786dff24f49dd87af0369921f43e4ee2a050
prathams177 <prathams177@gmail.com>
2021-05-07 20:26:42 +0530

    first commit

---
+++ screens/Camera.js
@@ -0,0 +1,99 @@
+import React,{useState} from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import {View,Text, StyleSheet,Image,Button, Alert} from 'react-native';
+import appimg from './appimg';
+
+const Camera=()=>{
+
+    const [image , setImage] = useState(null);
+    
+    const pickFromCamera =async()=>{
+        let result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing:true
+        })
+
+        if(!result.cancelled){
+            setImage(result.uri)
+        }
+    }
+
+    // loader
+    // const startloading=()=>{
+    //     setLoading(true);
+    //     setTimeout(()=>{
+    //         setLoading(false);
+    //     },3000)
+    // };
+
+    const changeimage = appimg[Math.floor(Math.random()* appimg.length)];
+                
+ return(
+        <>
+        <View style={styles.main}>
+       
+            <Button
+                title='click to add your image'
+                style={styles.bttn}
+                onPress={pickFromCamera}
+            />
+
+            <Image source={{uri:image}}   style={{width:200 ,height:200 , marginTop:30 , marginBottom:30 }}/>
+           
+            {image === null ?
+            (Alert.alert('add your image and see hows you looks like ')):
+            (<Image source= {changeimage} style={styles.img}/>)}
+
+            {image === null ?
+            (null) : 
+            (<Button title='try again' onPress={()=>{
+                setImage(null);
+                
+                
+            }}/>)}
+            
+            
+            
+
+           
+
+         </View>
+
+        </>
+    );
+
+}
+
+const styles=StyleSheet.create({
+
+
+    main:{
+        backgroundColor:'white',
+        alignItems:'center',
+        justifyContent:'center',
+        flex:1
+    },
+
+    img:{
+        height:200,
+        width:200,
+      
+     
+      }
+    
+
+    
+})
+
+export default Camera;
+
+
+
+
+
+
+
+
+
+
+
---
+++ screens/appimg.js
@@ -0,0 +1,16 @@
+const appimg =[
+    require('../assets/image1.jpeg'),
+    
+    require('../assets/image2.jpeg'),
+    
+    require('../assets/image3.jpeg'),
+    
+    require('../assets/image4.jpeg'),
+    
+    require('../assets/image5.jpeg'),
+    
+    require('../assets/image6.jpeg'),
+
+];
+
+export default appimg ;
